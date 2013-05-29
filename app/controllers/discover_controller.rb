0196b33e55a69eb981f07a4bf2f7ebd6297d8437
Pedro Karvinen <pedrokarvinen@gmail.com>
2013-05-29 18:19:33 +0300

    minor problems fixed

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -13,16 +13,38 @@ class DiscoverController < ApplicationController
   
   def brand
     @brand = Brand.find(:first, :conditions => "slug = '#{params[:brand]}'")
+    if not @brand
+      redirect_to "/discover/by-brand"
+    end
   end
   
   def initiative
     @brand = Brand.find(:first, :conditions => "slug = '#{params[:brand]}'")
+    if not @brand
+      redirect_to "/discover/by-brand"
+    end
+    
     @initiative = Initiative.find(:first, :conditions => "slug = '#{params[:initiative]}'")
+    if not @initiative
+      redirect_to "/discover/by-brand/" + params[:brand]
+    end
+    
   end
   
   def article
     @brand = Brand.find(:first, :conditions => "slug = '#{params[:brand]}'")
+    if not @brand
+      redirect_to "/discover/by-brand"
+    end
+    
     @initiative = Initiative.find(:first, :conditions => "slug = '#{params[:initiative]}'")
+    if not @initiative
+      redirect_to "/discover/by-brand/" + params[:brand]
+    end
+    
     @article = Article.find(:first, :conditions => "slug = '#{params[:article]}'")
+    if not @article
+      redirect_to "/discover/by-brand/" + params[:brand] + "/" + params[:initiative]
+    end
   end
 end
